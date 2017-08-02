9ab910ec13b822c709d3ddfde73439b67fee321b
Javier Moraga Rojas <jamoragar@iMac-de-Javier.local>
2017-08-02 11:55:59 -0300

    new - App en Ionic, SelkApp

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDBcXCY7k6wLEO526167NR59W3jKyFVKac",
+    authDomain: "selkapp-6180e.firebaseapp.com",
+    databaseURL: "https://selkapp-6180e.firebaseio.com",
+    projectId: "selkapp-6180e",
+    storageBucket: "selkapp-6180e.appspot.com",
+    messagingSenderId: "991526148135"
+  };
